0423f30f10f85bfdceb4df01fb0375690613195e
Tobias Hassebrock <tobias.hassebrock@gmail.com>
2025-06-17 12:31:51 +0200

    prettier setup

--- src/routes/files.tsx
+++ src/routes/files.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/files')({
-  component: Files,
+export const Route = createFileRoute('/files')({  component: Files,
 })
 
 function Files() {
